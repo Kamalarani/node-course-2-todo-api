b6fa82b9cb0e67dc1e39abb4e281054ada371c94
Kamalarani Pichaimuneswaran <EVOLVUS\kamalaranip@EPCBLR-044.evolvus.com>
2018-04-10 17:13:53 +0530

    ADD PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,5 +100,30 @@ it("Should return 404 if wrong url is found",(done) => {
   .expect(404)
   .end(done);
 });
+});
 
+describe("DELETE /todos/:id", () => {
+  it("should remove a todo",(done) => {
+    var hexId = todos[1].id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err,res) => {
+      if(err){
+        return done(err);
+      }
+
+      Todo.findById(hexId).then((todo)=>{
+        expect(todo).toNotExist();
+        done();
+        console.log("The Document is deleted successfully");
+      }).catch((err)=>{
+        done(err);
+      });
+    });
+  });
 });
